f09594da0693d243e9ae9b07e0fafba7e3b3db96
ShikharBind <shikhar.bind2010@gmail.com>
2023-09-05 04:56:04 +0530

    Polled Input

--- Scotch/src/Scotch/Application.cpp
+++ Scotch/src/Scotch/Application.cpp
@@ -5,6 +5,8 @@
 
 #include "glad/glad.h"
 
+#include "Input.h"
+
 namespace Scotch {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -13,7 +15,7 @@ namespace Scotch {
 
 	Application::Application() 
 	{
-		SH_CORE_ASSERT(s_Instance, "Application already exist");
+		SH_CORE_ASSERT(!s_Instance, "Application already exist");
 		s_Instance = this;
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
@@ -53,6 +55,9 @@ namespace Scotch {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			SH_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
--- Scotch/src/Scotch/Core.h
+++ Scotch/src/Scotch/Core.h
@@ -16,7 +16,7 @@
 
 #ifdef SH_ENABLE_ASSERTS
 #define SH_ASSERT(x, ...) { if(!(x)) {SH_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define SH_CORE_ASSERT(x, ...) { if(!(x)) {SH_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
+#define SH_CORE_ASSERT(x, ...) { if(!(x)) {SH_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 	#define SH_ASSERT(x, ...)
 	#define SH_CORE_ASSERT(x, ...)
